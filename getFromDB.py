ea81f7c1910e2893495df55cf6e65b1447fee0fa
Ne kit <chiziwe@ya.ru>
2020-07-11 15:06:09 +0300

    v1.4

--- getFromDB.py
+++ getFromDB.py
@@ -15,14 +15,16 @@ def return_all_ishod(date_start, time_start, date_end, time_end):
     dt_end = DT.datetime.fromisoformat(date_end + ' ' + time_end)
 
     packs = BD.query(sql="SELECT frametime, size FROM netsessions " +
-                         "WHERE (frametime >= " + str(dt_start.timestamp()) + " AND frametime <= " +
+                         "WHERE (frametime >= " + str(dt_start.timestamp() + 10800) + " AND frametime <= " +
                          str(
-                             dt_end.timestamp()) + " AND srcip like '10.227.11.%' AND dstport='53') ORDER BY frametime;")
-    time, size = [], []
-    for i in range(len(packs)):
-        time.append(packs[i][0])
-        size.append(packs[i][1])
-    return time, size
+                             dt_end.timestamp() + 10800) + " AND srcip like '10.227.11.%' AND dstport='53') ORDER BY frametime;")
+
+    quantity = BD.query(sql="SELECT count(*) FROM netsessions " +
+                            "WHERE (frametime >= " + str(dt_start.timestamp() + 10800) + " AND frametime <= " +
+                            str(
+                                dt_end.timestamp() + 10800) + " AND srcip like '10.227.11.%' AND dstport='53') ORDER BY frametime;")
+
+    np.save('Исходящий (' + date_start + ' ' + time_start + '; ' + date_end + ' ' + time_end + ')', packs)
 
 
 def return_all_vhod(date_start, time_start, date_end, time_end):
@@ -30,16 +32,23 @@ def return_all_vhod(date_start, time_start, date_end, time_end):
     dt_end = DT.datetime.fromisoformat(date_end + ' ' + time_end)
 
     packs = BD.query(sql="SELECT frametime, size FROM netsessions " +
-                         "WHERE (frametime >= " + str(dt_start.timestamp()) + " AND frametime <= " +
+                         "WHERE (frametime >= " + str(dt_start.timestamp() + 10800) + " AND frametime <= " +
                          str(
-                             dt_end.timestamp()) + " AND dstip like '10.227.11.%' AND srcport='53') ORDER BY frametime;")
+                             dt_end.timestamp() + 10800) + " AND dstip like '10.227.11.%' AND srcport='53') ORDER BY frametime;")
 
-    time, size = [], []
-    for i in range(len(packs)):
-        time.append(packs[i][0])
-        size.append(packs[i][1])
+    quantity = BD.query(sql="SELECT count(*) FROM netsessions " +
+                            "WHERE (frametime >= " + str(dt_start.timestamp() + 10800) + " AND frametime <= " +
+                            str(
+                                dt_end.timestamp() + 10800) + " AND dstip like '10.227.11.%' AND srcport='53') ORDER BY frametime;")
 
-    return time, size
+    # time, size = [], []
+    # for i in range(len(packs)):
+    #     time.append(packs[i][0])
+    #     size.append(packs[i][1])
+    #
+    # return time, size
+
+    np.save('Входящий (' + date_start + ' ' + time_start + '; ' + date_end + ' ' + time_end + ')', packs)
 
 
 def return_avg_packet_ishod(date_start, time_start, date_end, time_end):
@@ -47,8 +56,8 @@ def return_avg_packet_ishod(date_start, time_start, date_end, time_end):
     dt_end = DT.datetime.fromisoformat(date_end + ' ' + time_end)
 
     packs = BD.query(sql="SELECT avg(size) AS avgfs FROM netsessions " +
-                         "WHERE (frametime >= " + str(dt_start.timestamp()) + " AND frametime <= " +
-                         str(dt_end.timestamp()) + " AND srcip like '10.227.11.%' AND dstport='53');")
+                         "WHERE (frametime >= " + str(dt_start.timestamp() + 10800) + " AND frametime <= " +
+                         str(dt_end.timestamp() + 10800) + " AND srcip like '10.227.11.%' AND dstport='53');")
     return packs
 
 
@@ -57,8 +66,8 @@ def return_avg_packet_vhod(date_start, time_start, date_end, time_end):
     dt_end = DT.datetime.fromisoformat(date_end + ' ' + time_end)
 
     packs = BD.query(sql="SELECT avg(size) AS avgfs FROM netsessions " +
-                         "WHERE (frametime >= " + str(dt_start.timestamp()) + " AND frametime <= " +
-                         str(dt_end.timestamp()) + " AND dstip like '10.227.11.%' AND srcport='53');")
+                         "WHERE (frametime >= " + str(dt_start.timestamp() + 10800) + " AND frametime <= " +
+                         str(dt_end.timestamp() + 10800) + " AND dstip like '10.227.11.%' AND srcport='53');")
     return packs
 
 
@@ -66,28 +75,30 @@ def ping(date_start, time_start, date_end, time_end):
     dt_start = DT.datetime.fromisoformat(date_start + ' ' + time_start)
     dt_end = DT.datetime.fromisoformat(date_end + ' ' + time_end)
 
-    packs = BD.query(sql="SELECT frametime, size, dstip, dstport, srcip, srcport FROM netsessions " +
-                         "WHERE (frametime >= " + str(dt_start.timestamp()) + " AND frametime <= " +
-                         str(dt_end.timestamp()) + " AND (srcip LIKE '10.227.11.%' OR dstip LIKE '10.227.11.%') " +
-                         "AND (dstport='53' OR srcport='53')) ORDER BY frametime;")
+    packs = BD.query(sql="SELECT * FROM netsessions " +
+                         "WHERE (frametime >= " + str(dt_start.timestamp() + 10800) + " AND frametime <= " +
+                         str(
+                             dt_end.timestamp() + 10800) + " AND srcip like '10.227.11.%' AND dstport='53') ORDER BY frametime;")
 
-    time, size, dstip, dstport, srcip, srcport = [], [], [], [], [], []
+    # np.save('Исходящий (для поиска времени "запрос-ответ")', packs)
+
+    delta = 3
+    response_time_arr = []
+
     for i in range(len(packs)):
-        dip = packs[i][2]
-        dport = packs[i][3]
-        while
+        out_time = float(packs[i][1])
+        sip = str(packs[i][2])
+        sport = str(packs[i][3])
 
+        in_time = out_time + delta
 
+        answer = BD.query(sql="SELECT * FROM netsessions " +
+                              "WHERE (frametime >= " + str(out_time) + " AND frametime <= " +
+                              str(
+                                  in_time) + " AND dstip = '" + str(sip.replace(' ', '')) + "' AND dstport= '" + str(
+            sport.replace(' ', '')) + "') ORDER BY frametime;")
 
-        time.append(packs[i][0])
-        size.append(packs[i][1])
-        dstip.append(packs[i][2])
-        dstport.append(packs[i][3])
-        srcip.append(packs[i][4])
-        srcport.append(packs[i][5])
+        response_time = float(answer[0][1]) - out_time
+        response_time_arr.append(response_time)
 
-        all = np.concatenate([time, size, dstip, dstport, srcip, srcport])
-
-#    for i in range(len(time)):
-
-    return all #, size, dstip, dstport, srcip, srcport
+    return response_time_arr
--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-db.py
+import numpy as np
+import datetime as DT
+ds = '2020-07-08'
+ts = '15:25:00'
+de = '2020-07-08'
+te = '15:55:00'
+
+a = np.load('packs.npy')
+
+print(len(a))
+
